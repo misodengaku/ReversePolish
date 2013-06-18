aab0fe8eb6afed699e2193954ea1520c979cb76f
misodengaku <misodengaku@gmail.com>
2013-06-18 19:46:27 +0900

    first commit

---
+++ reverse_polish/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reverse_polish
+{
+
+    public partial class Form1 : Form
+    {
+        ReversePolish rp = new ReversePolish();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label1.Text = "" + rp.ExecutePolish(rp.GetPolishString(textBox1.Text));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rp.Variables[textBox2.Text] = int.Parse(textBox3.Text);
+            listBox1.Items.Add("変数セット：" + textBox2.Text + "=" + textBox3.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("変数内容：" + textBox2.Text + "=" + rp.Variables[textBox2.Text]);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "" + rp.isVariable(textBox1.Text);
+        }
+    }
+}
---
+++ reverse_polish/ReversePolish.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_polish
+{
+    public class ReversePolish
+    {
+        //TODO: 数式で代入できるようにしてreadonlyな感じにする
+        public Dictionary<string, int> Variables = new Dictionary<string, int>();
+
+        /// <summary>
+        /// 数式を逆ポーランド記法に変換します。
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public string GetPolishString(string s)
+        {
+            string ret = "";
+            var stack = new Stack<char>();
+            foreach (var i in s)
+            {
+                if (i == '+' || i == '-' || i == '*' || i == '/' || i == '=')
+                {
+                    ret += ",";
+                    stack.Push(i);
+                }
+                else
+                    ret += i;
+
+            }
+            for (int i = stack.Count() - 1; i >= 0; i--)
+            {
+                ret += ",";
+                ret += stack.Pop();
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// カンマ区切りの逆ポーランド記法で表された数式を実行し、計算結果を返します
+        /// </summary>
+        /// <param name="s">数式</param>
+        /// <returns>計算結果</returns>
+        public int ExecutePolish(string s)
+        {
+            try
+            {
+                var stack = new Stack<string>();
+                string n1, n2;
+
+                foreach (var i in s.Split(new char[] { ',' }))
+                {
+                    if (i[0] >= '0' && i[0] <= '9')
+                    {
+                        stack.Push("" + i);
+                    }
+                    else if (i == "+" || i == "-" || i == "*" || i == "/" || i == "=")
+                    {
+                        int r = 0;
+                        n2 = stack.Pop();
+                        n1 = stack.Pop();
+                        switch (i)
+                        {
+                            case "+":
+                                r = int.Parse(n1) + int.Parse(n2);
+                                break;
+                            case "-":
+                                r = int.Parse(n1) - int.Parse(n2);
+                                break;
+                            case "*":
+                                r = int.Parse(n1) * int.Parse(n2);
+                                break;
+                            case "/":
+                                r = int.Parse(n1) / int.Parse(n2);
+                                break;
+                            case "=":
+                                r = int.Parse(n2);
+                                n1 = n2;
+                                break;
+                        }
+                        stack.Push("" + r);
+                    }
+                    else
+                    {
+                        stack.Push("" + Variables[i]);
+                    }
+                }
+                return int.Parse(stack.Pop());
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        /// 変数を指す文字列かどうかを判定します
+        /// </summary>
+        /// <param name="s">判定対象の文字列</param>
+        /// <returns>変数であればtrue</returns>
+        public bool isVariable(string s)
+        {
+            return (s != "+") && (s != "-") && (s != "*") && (s != "/") && !(s[0] >= '0' && s[0] <= '9');
+        }
+
+    }
+}
